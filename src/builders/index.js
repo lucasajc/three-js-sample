98a0a62a505ef1dd44726e07ae65a1ab48bc0fae
Lucas de Aguilar <lucasdeaguilarjc@hotmail.com>
2022-06-20 21:17:46 +0200

    Init project

---
+++ src/builders/index.js
@@ -0,0 +1,5 @@
+export { default as LightBuilder } from "./light.builder";
+export { default as PlaneBuilder } from "./plane.builder";
+export { default as SphereBuilder } from "./sphere.builder";
+export { default as CubeBuilder } from "./cube.builder";
+export { default as LineBuilder } from "./line.builder";
---
+++ src/paths/particleInsideSphere.js
@@ -0,0 +1,36 @@
+const getRandomArbitrary = (minimum, maximum) => {
+  return Math.random() * (maximum - minimum) + minimum;
+}
+
+const r = (a = 0, b = 0, c = 0) => {
+  const k = Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2);
+  return Math.sqrt(k);
+}
+
+class ParticleInsideSpherePath {
+  speed;
+
+  constructor(particle, sphere) {
+    this.particle = particle;
+    this.sphere = sphere;
+    this.speed = { x: 0, y: 0, z: 0 };
+  }
+
+  createInitialPath() {
+    this.speed.x = getRandomArbitrary(-0.1, 0.1);
+    this.speed.y = getRandomArbitrary(-0.1, 0.1);
+    this.speed.z = getRandomArbitrary(-0.1, 0.1);
+  }
+
+  run() {
+    const radius = r(this.particle.position.x, this.particle.position.y, this.particle.position.z);
+    if (radius >= (this.sphere.geometry.parameters.radius - this.particle.geometry.parameters.radius * 2)) {
+      this.speed.x = -this.speed.x;
+      this.speed.y = -this.speed.y;
+      this.speed.z = -this.speed.z;
+    }
+    this.particle.move({ x: this.speed.x, y: this.speed.y, z: this.speed.z });
+  }
+}
+
+export default ParticleInsideSpherePath;
